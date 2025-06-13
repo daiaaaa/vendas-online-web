5c36c29fc34711c240ec4e0b3b721c76149132d9
Daiana <daianavieiradeandrade@gmail.com>
2025-06-13 09:34:41 -0300

    feat: create login component

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,9 +1,18 @@
-import { BackgroundImage } from "../styles/loginScreen.styles";
+import { BackgroundImage, LimitedContainer, LogoImage } from '../styles/loginScreen.styles';
+import { ContainerLogin } from '../styles/loginScreen.styles';
+import { ContainerLoginScreen } from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
-  return (<div>
-    <BackgroundImage src="background.png"/>
-  </div>)
-}
+  return (
+    <ContainerLoginScreen>
+      <ContainerLogin>
+        <LimitedContainer>
+          <LogoImage src="./logo.png" />
+        </LimitedContainer>
+      </ContainerLogin>
+      <BackgroundImage src="background.png" />
+    </ContainerLoginScreen>
+  );
+};
 
 export default LoginScreen;
--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+export const ContainerLoginScreen = styled.div`
+    display: flex;
+    justify-content: right;
+    width: 100%;
+`;
+
 export const BackgroundImage = styled.img`
     position: absolute;
     top: 0;
@@ -7,4 +13,29 @@ export const BackgroundImage = styled.img`
     width: 100%;
     height: 100vh;
     object-fit: cover;
-    `;
+    z-index: -1;
+ `;
+
+export const LogoImage = styled.img`
+    width: 100%;
+
+
+`;
+
+export const ContainerLogin = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(255, 255, 255);
+    padding: 22px;
+    width: 100%;
+    height: 100vh;
+    max-width: 646px;
+`;
+
+export const LimitedContainer = styled.div`
+    width: 100%;
+    max-width: 498px;
+    display: flex;
+    justify-content: center;
+`;
